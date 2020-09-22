56993d6e4c999cf2288c2794770ef71d126987b6
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2020-09-22 23:40:53 +0300

    showName() method

--- task02/src/by/tsvirko/task02/task1/Hello.java
+++ task02/src/by/tsvirko/task02/task1/Hello.java
@@ -7,6 +7,10 @@ public class Hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
+        showHello(name);
+    }
+
+    private static void showHello(String name) {
         ValidateName validateName = new ValidateName();
         if (validateName.validateUserName(name)) {
             String nameLang = validateName.chooseLanguage(name);
