7424c19bcf02f04f48db382d879387675a74303b
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2020-09-23 20:38:17 +0300

    task7

---
+++ task02/src/by/tsvirko/task02/task7/BirthdayMain.java
@@ -0,0 +1,41 @@
+package by.tsvirko.task02.task7;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class BirthdayMain {
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter birthday date like " + "'" + DATE_FORMAT + "'");
+        String userDate = scanner.next();
+        validateDateFormat(userDate);
+        Date javaDate = new SimpleDateFormat(DATE_FORMAT).parse(userDate);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(javaDate);
+        System.out.println(weekDay(userDate, cal));
+        BirthdayUtility.showFullAge(javaDate, cal);
+        if (BirthdayUtility.isBirthday(javaDate, cal)) System.out.println("Happy birthday!!!");
+    }
+
+    private static boolean validateDateFormat(String strDate) {
+        SimpleDateFormat sdfrmt = new SimpleDateFormat(DATE_FORMAT);
+        sdfrmt.setLenient(false);
+        try {
+            sdfrmt.parse(strDate);
+        } catch (ParseException e) {
+            System.err.println(strDate + " is Invalid Date format");
+            return false;
+        }
+        return true;
+    }
+
+    private static String weekDay(String userDate, Calendar calendar) {
+        String[] days = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        return days[calendar.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+}
---
+++ task02/src/by/tsvirko/task02/task7/BirthdayUtility.java
@@ -0,0 +1,68 @@
+package by.tsvirko.task02.task7;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+public class BirthdayUtility {
+    private BirthdayUtility() {
+        throw new AssertionError("Instantiating utility class.");
+    }
+
+    public static void showFullAge(Date userDate, Calendar calendar) throws ParseException {
+        int years = 0;
+        int months = 0;
+        int days = 0;
+
+        Calendar birthDay = Calendar.getInstance();
+        birthDay.setTimeInMillis(userDate.getTime());
+
+        long currentTime = System.currentTimeMillis();
+        Calendar now = Calendar.getInstance();
+        now.setTimeInMillis(currentTime);
+
+        years = now.get(Calendar.YEAR) - birthDay.get(Calendar.YEAR);
+        int currMonth = now.get(Calendar.MONTH) + 1;
+        int birthMonth = birthDay.get(Calendar.MONTH) + 1;
+
+        months = currMonth - birthMonth;
+
+        if (months < 0) {
+            years--;
+            months = 12 - birthMonth + currMonth;
+            if (now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
+                months--;
+        } else if (months == 0 && now.get(Calendar.DATE) < birthDay.get(Calendar.DATE)) {
+            years--;
+            months = 11;
+        }
+
+        if (now.get(Calendar.DATE) > birthDay.get(Calendar.DATE))
+            days = now.get(Calendar.DATE) - birthDay.get(Calendar.DATE);
+        else if (now.get(Calendar.DATE) < birthDay.get(Calendar.DATE)) {
+            int today = now.get(Calendar.DAY_OF_MONTH);
+            now.add(Calendar.MONTH, -1);
+            days = now.getActualMaximum(Calendar.DAY_OF_MONTH) - birthDay.get(Calendar.DAY_OF_MONTH) + today;
+        } else {
+            days = 0;
+            if (months == 12) {
+                years++;
+                months = 0;
+            }
+        }
+        System.out.println(years + " years " + months + " months " + days + " days");
+    }
+
+    public static boolean isBirthday(Date usersDate, Calendar calendar) {
+        int usersDay = calendar.get(Calendar.DATE);
+        int usersMonth = calendar.get(Calendar.MONTH);
+        int usersYear = calendar.get(Calendar.YEAR);
+
+        Calendar now = Calendar.getInstance();
+        int nowDay = now.get(Calendar.DATE);
+        int nowMonth = now.get(Calendar.MONTH);
+        int nowYear = now.get(Calendar.YEAR);
+
+        return (usersDay == nowDay) && (usersMonth == nowMonth) && (usersYear == nowYear);
+    }
+}
