90f52bacd6f160a386acc5b47f10c2839702ab79
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2020-09-25 22:50:17 +0300

    Ball price counter was added

--- task03/src/by/tsvirko/task03/BallColour.java
+++
@@ -1,5 +0,0 @@
-package by.tsvirko.task03;
-
-public enum BallColour {
-    BLUE, GREEN, PURPLE, RED;
-}
---
+++ task03/src/by/tsvirko/task03/service/BallPriceCounter.java
@@ -0,0 +1,23 @@
+package by.tsvirko.task03.service;
+
+public class BallPriceCounter {
+    private double blueCoeff = 1.2;
+    private double redCoeff = 1.3;
+    private double greenCoeff = 1.4;
+    private double purpleCoeff = 1.5;
+
+    public double countPrice(BallColour colour, double weight) {
+        switch (colour) {
+            case BLUE:
+                return weight * blueCoeff;
+            case RED:
+                return weight * redCoeff;
+            case GREEN:
+                return weight * greenCoeff;
+            case PURPLE:
+                return weight * purpleCoeff;
+            default:
+                return 0;
+        }
+    }
+}
