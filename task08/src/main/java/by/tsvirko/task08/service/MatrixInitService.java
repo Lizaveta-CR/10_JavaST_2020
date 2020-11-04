6397f96de31b3ae17d6d3d0c3b8c5c47dd4d2826
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2020-11-04 09:55:32 +0300

    init matrix from file with boundaries

--- task08/src/main/java/by/tsvirko/task08/service/MatrixInitService.java
+++ task08/src/main/java/by/tsvirko/task08/service/MatrixInitService.java
@@ -2,7 +2,8 @@ package by.tsvirko.task08.service;
 
 import by.tsvirko.task08.entity.Matrix;
 import by.tsvirko.task08.entity.exception.MatrixException;
+import by.tsvirko.task08.service.exception.ServiceInitException;
 
 public interface MatrixInitService {
-    Matrix init(String fileName) throws MatrixException;
+    Matrix init(String fileName, int n1, int n2) throws MatrixException, ServiceInitException;
 }
--- task08/src/main/java/by/tsvirko/task08/service/MatrixInitServiceImpl.java
+++ task08/src/main/java/by/tsvirko/task08/service/MatrixInitServiceImpl.java
@@ -2,25 +2,38 @@ package by.tsvirko.task08.service;
 
 import by.tsvirko.task08.entity.Matrix;
 import by.tsvirko.task08.entity.exception.MatrixException;
+import by.tsvirko.task08.service.exception.ServiceInitException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 
 public class MatrixInitServiceImpl implements MatrixInitService {
     private static final Logger logger = LogManager.getLogger(MatrixInitServiceImpl.class);
 
+    /**
+     * Method to read matrix from file
+     *
+     * @param fileName
+     * @param n1       - boundary
+     * @param n2       - boundary
+     * @return Matrix
+     * @throws MatrixException
+     * @throws ServiceInitException
+     */
     @Override
-    public Matrix init(String fileName) throws MatrixException {
+    public Matrix init(String fileName, int n1, int n2) throws MatrixException, ServiceInitException {
+        if (!new Validator().validate(n1, n2)) {
+            throw new ServiceInitException("Wrong parameters");
+        }
         URI uri = null;
         try {
             uri = ClassLoader.getSystemResource("data").toURI();
@@ -29,38 +42,38 @@ public class MatrixInitServiceImpl implements MatrixInitService {
         }
         String mainPath = Paths.get(uri).toString();
         Path path = Paths.get(mainPath, fileName);
-        List<List<Integer>> list = new ArrayList<>();
         Scanner input = null;
         try {
-            input = new Scanner(new File(String.valueOf(path)));
+            input = new Scanner(new BufferedReader(new FileReader(String.valueOf(path))));
         } catch (FileNotFoundException e) {
             logger.info("No such file exception");
-//            throw new ServiceInitException()
+            throw new ServiceInitException("No such file");
         }
-        while (input.hasNextLine()) {
-            List<Integer> row = createRow(input.nextLine());
-            list.add(row);
+        int rows = 0;
+
+        if (n1 > n2) {
+            rows = n2 + (int) (Math.random() * ((n1 - n2) + 1));
+        } else if (n1 < n2) {
+            rows = n1 + (int) (Math.random() * ((n2 - n1) + 1));
+        } else {
+            rows = n1;
         }
 
-        int rows = list.size();
-        int columns = list.get(0).size();
+        int columns = rows;
         Matrix matrix = new Matrix(rows, columns);
-        for (int i = 0; i < matrix.getVerticalSize(); i++) {
-            for (int j = 0; j < matrix.getHorizontalSize(); j++) {
-                int value = list.get(i).get(j);
-                matrix.setElement(i, j, value);
+        try {
+            while (input.hasNextLine()) {
+                String nextLine = input.nextLine();
+                for (int i = 0; i < matrix.getVerticalSize(); i++) {
+                    String[] line = nextLine.trim().split(" ");
+                    for (int j = 0; j < matrix.getHorizontalSize(); j++) {
+                        matrix.setElement(i, j, Integer.parseInt(line[j]));
+                    }
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ServiceInitException("Too big input values");
         }
         return matrix;
-    }
-
-    private static List<Integer> createRow(String line) {
-        List<Integer> elements = new ArrayList<>();
-        String[] strings = line.split(" ");
-        for (int i = 0; i < strings.length; i++) {
-            String trim = strings[i].trim();
-            elements.add(i, Integer.parseInt(trim));
-        }
-        return elements;
     }
 }
