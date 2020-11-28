413a0eb1f1bb72f0bae89bdac9fe2e0052fdb55b
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2020-11-28 17:57:13 +0300

    STAXBuilderOrdersTest

---
+++ part2task03/src/test/java/part2task04/service/builder/builderOrders/STAXBuilderOrdersTest.java
@@ -0,0 +1,31 @@
+package part2task04.service.builder.builderOrders;
+
+import by.tsvirko.entity.orders.OrderEnum;
+import by.tsvirko.service.bulders.BaseBuilder;
+import by.tsvirko.service.bulders.builderOrders.SAXBuilderOrders;
+import by.tsvirko.service.bulders.builderOrders.STAXBuilderOrders;
+import by.tsvirko.service.parser.DOMParser;
+import by.tsvirko.service.parser.PathContainer;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+
+
+public class STAXBuilderOrdersTest {
+    private BaseBuilder builder = new STAXBuilderOrders();
+
+    @Test(description = "Testing SAXBuilder' buildOrders() method")
+    public void testBuildSize() {
+        builder.build();
+        int size = builder.getItems().size();
+        try {
+
+            Document doc = new DOMParser().parse(PathContainer.ORDERS_XML, PathContainer.ORDERS_XSD);
+
+            int actualLength = doc.getElementsByTagName(OrderEnum.ORDER.getField()).getLength();
+
+            Assert.assertEquals(size, actualLength);
+        } catch (Exception e) {
+        }
+    }
+}
